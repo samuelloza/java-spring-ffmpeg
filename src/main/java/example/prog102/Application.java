4f4f147c52f9c3cfdea75c24a04dc7a550b1674a
Samuel Loza <starsaminf@gmail.com>
2020-09-01 21:47:41 -0400

    CheckStyle

--- src/main/java/example/prog102/Application.java
+++ src/main/java/example/prog102/Application.java
@@ -9,7 +9,7 @@ import org.springframework.context.annotation.ComponentScan;
 @EnableAutoConfiguration
 public class Application extends SpringBootServletInitializer {
 
-    public static void main(String[] args) {
+    public static void main(final String[] args) {
         SpringApplication.run(Application.class, args);
     }
 }
--- src/main/java/example/prog102/controllers/WelcomeController.java
+++ src/main/java/example/prog102/controllers/WelcomeController.java
@@ -2,22 +2,11 @@ package example.prog102.controllers;
 
 import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.nio.charset.Charset;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.nio.file.StandardCopyOption;
-import java.text.DateFormat;
-import java.text.SimpleDateFormat;
-import java.util.Date;
-import java.util.Map;
-
-import com.fasterxml.jackson.databind.node.ObjectNode;
-
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.util.SocketUtils;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -26,42 +15,44 @@ import org.springframework.web.multipart.MultipartFile;
 
 @RestController
 public class WelcomeController {
-	public final static String EXECUTABLE = "/usr/bin/ffmpeg";
+  static final String EXECUTABLE_NAME = "/usr/bin/ffmpeg";
 
-	@Value("${upload.path}")
-	private String path;
+  @Value("${upload.path}")
+  private String path;
 
-	@Value("${storage.path}")
-	private String storage;
+  @Value("${storage.path}")
+  private String storage;
+  /**
+  * displays hello world without param
+  */
 
-	@RequestMapping(method = RequestMethod.POST, value = "/video-converter")
+  @RequestMapping(method = RequestMethod.POST, value = "/video-converter")
+  public String sayHelloPostAtributesWithFile(@RequestParam("file") final MultipartFile file,
+      @RequestParam("comands") final String comads) {
+    String fileName = "";
+    String source = "";
+    String target = "";
+    try {
+      fileName = file.getOriginalFilename();
+      source = path + fileName;
+      target = storage + file.getOriginalFilename().replaceFirst("[.][^.]+$", "") + ".avi";
+      Files.copy(file.getInputStream(), Paths.get(source), StandardCopyOption.REPLACE_EXISTING);
+    } catch (IOException e) {
+      return "Ups error to obtain data of the file";
+    }
 
-	public String sayHelloPostAtributesWithFile(@RequestParam("file") MultipartFile file,
-			@RequestParam("comands") String comads) {
-		String fileName = "";
-		String source = "";
-		String target = "";
-		try {
-			fileName = file.getOriginalFilename();
-			source = path + fileName;
-			target = storage + file.getOriginalFilename().replaceFirst("[.][^.]+$", "") + ".avi";
-			Files.copy(file.getInputStream(), Paths.get(source), StandardCopyOption.REPLACE_EXISTING);
-		} catch (IOException e) {
-			return "Ups error to obtain data of the file";
-		}
-
-		try {
-			Process processDuration = new ProcessBuilder("ffmpeg", "-i", source, target).redirectErrorStream(true).start();
-			StringBuilder outPut = new StringBuilder();
-			BufferedReader processOutputReader = new BufferedReader(new InputStreamReader(processDuration.getInputStream()));
-			String line;
-			while ((line = processOutputReader.readLine()) != null) {
-				outPut.append(line + System.lineSeparator());
-			}
-			processDuration.waitFor();
-			return outPut.toString().trim();
-		} catch (Exception e) {
-			return "Error to convert file";
-		}
-	}
+    try {
+      Process processDuration = new ProcessBuilder(EXECUTABLE_NAME, "-i", source, target).redirectErrorStream(true).start();
+      StringBuilder outPut = new StringBuilder();
+      BufferedReader processOutputReader = new BufferedReader(new InputStreamReader(processDuration.getInputStream()));
+      String line;
+      while ((line = processOutputReader.readLine()) != null) {
+        outPut.append(line + System.lineSeparator());
+      }
+      processDuration.waitFor();
+      return outPut.toString().trim();
+    } catch (Exception e) {
+      return "Error to convert file";
+    }
+  }
 }
